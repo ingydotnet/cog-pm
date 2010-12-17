f975d469a59302afee3bcd0118d0d86d298cc36e
Ingy dot Net <ingy@ingy.net>
2010-12-17 17:05:40 +1100

    ...

---
+++ share/static/js/sidepanel.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("li")
+        .mouseenter(function() {
+            $(this).find('ul').show(300);
+        });
+    $(".sidebar")
+        .mouseleave(function() {
+            $(this).find('li ul').hide(500);
+        });
+});
