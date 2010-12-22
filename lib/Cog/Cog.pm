662432ff8f4f8f245cbe48ddb82bbbe4270326b7
Ingy dot Net <ingy@ingy.net>
2010-12-23 00:25:42 +1100

    add COg::Cg

---
+++ lib/Cog/Cog.pm
@@ -0,0 +1,9 @@
+package Cog::Cog;
+
+our $VERSION = '0.05';
+
+# This module is here for a CPAN indexing point. Cog is currently
+# indexed to CNC::Cog. Cog based modules should list Cog::Cog as the
+# prerequisite module for now.
+
+1;
