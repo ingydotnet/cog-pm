95b0b33f433ce369180a6df0fccda8b73b66a4d4
Ingy dot Net <ingy@ingy.net>
2011-01-23 16:07:08 +1100

    Move much maker code to cogwiki

--- lib/Cog/Maker.pm
+++ lib/Cog/Maker.pm
@@ -24,9 +24,21 @@ has json => ('is' => 'ro', builder => sub {
 sub make {
     my $self = shift;
     $self->config->chdir_root();
+    $self->make_cache;
+    $self->make_layout;
+    $self->make_config_js();
+    $self->make_url_map_js();
+    $self->make_all_js();
+    $self->make_css();
+}
 
-    # Crea
+sub make_cache {
+    my $self = shift;
     io('cache')->mkdir;
+}
+
+sub make_layout {
+    my $self = shift;
     my $data = +{%{$self->config}};
     my $html = tt()
         ->path(['template/'])
@@ -34,52 +46,6 @@ sub make {
         ->post_chomp
         ->render('layout.html.tt');
     io('cache/layout.html')->print($html);
-
-    $self->make_config_js();
-    $self->make_url_map_js();
-
-    my $time = time;
-    my $page_list = [];
-    my $blobs = {};
-
-    $self->config->store->delete_tag_index; # XXX Temporary solution until can do smarter
-    for my $page_file (io($self->config->content_root)->all_files) {
-        next unless $page_file->filename =~ /\.cog$/;
-        my $page = $self->config->classes->{page}->from_text($page_file->all);
-        my $id = $page->Short or next;
-
-        for my $Name (@{$page->Name}) {
-            my $name = $self->config->store->index_name($Name, $id);
-            io->file("cache/name/$name.txt")->assert->print($id);
-        }
-
-        $self->config->store->index_tag($_, $id)
-            for $self->all_tags($page);
-            
-        my $blob = {
-            %$page,
-            Id => $id,
-            Type => $page->Type,
-            Title => $page->Title,
-            Time => $page->Time,
-            size => length($page->Content),
-            duration => $page->duration,
-        };
-        delete $blob->{Content};
-        delete $blob->{Name};
-        push @$page_list, $blob;
-        $blobs->{$id} = $blob;
-
-        $self->make_page_html($page, $page_file);
-
-        delete $page->{content};
-        io("cache/$id.json")->print($self->json->encode($blob));
-    }
-    io("cache/page-list.json")->print($self->json->encode($page_list));
-
-    $self->make_tag_cloud($blobs);
-    $self->make_js();
-    $self->make_css();
 }
 
 sub all_tags {
@@ -130,70 +96,7 @@ sub make_tag_cloud {
     io("cache/tag-list.json")->print($self->json->encode([sort keys %$tags]));
 }
 
-sub make_page_html {
-    my $self = shift;
-    my $page = shift;
-    my $page_file = shift;
-    my $id = $page->Short;
-    my $html_filename = "cache/$id.html";
-
-    return if -e $html_filename and -M $html_filename < -M $page_file->name;
-
-    my $markup = $page->markup;
-
-    my $method =
-        $markup eq 'pod' ? 'make_pod_html' :
-        $markup eq 'asc' ? 'make_asc_html' :
-        $markup eq 'txt' ? 'make_txt_html' :
-        'make_txt_html';
-
-    my $html = $self->$method($page);
-
-    print $page_file->filename . " -> $html_filename\n";
-    io($html_filename)->assert->print($html);
-}
-
-sub make_txt_html {
-    my $self = shift;
-    my $page = shift;
-    my $html = $page->Content;
-
-    $html =~ s/&/&amp;/g;
-    $html =~ s/</&lt;/g;
-    $html =~ s/>/&gt;/g;
-
-    $html = "<pre>$html</pre>\n";
-
-    return $html;
-}
-
-sub make_asc_html {
-    my $self = shift;
-    my $page = shift;
-
-    my ($in, $out, $err) = ($page->Content, '', '');
-    my @cmd = qw(asciidoc -s -);
-    IPC::Run::run(\@cmd, \$in, \$out, \$err, IPC::Run::timeout(30));
-
-    return $out;
-}
-
-sub make_pod_html {
-    my $self = shift;
-    my $page = shift;
-
-    my $html;
-    my $pod = $page->Content;
-    my $p = Pod::Simple::HTML->new;
-    $p->output_string(\$html);
-    $p->parse_string_document($pod);
-
-    $html =~ s/.*!-- start doc -->(.*?)<!-- end doc --.*/$1/s or die;
-
-    return $html;
-}
-
-sub make_js {
+sub make_all_js {
     my $self = shift;
     my $root = $self->config->root_dir;
     my $js = "$root/static/js";
@@ -206,7 +109,7 @@ sub make_js {
         ->render('js-mf.mk.tt');
     io("$js/Makefile")->print($makefile);
 
-    system("(cd $js; make)") == 0 or die;
+    system("(cd $js; make all.js)") == 0 or die;
 }
 
 sub make_css {
