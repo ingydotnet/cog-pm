f542808b4a150116524fccb85527d13f16af8b54
Ingy dot Net <ingy@ingy.net>
2011-02-17 21:17:50 -0800

    copy pasta

--- lib/Cog/Base.pm
+++ lib/Cog/Base.pm
@@ -11,8 +11,8 @@ sub initialize { $config ||= $_[1] }
 
 sub config { $config }
 sub maker { $maker || ($maker = $config->maker) }
-sub runner { $runner || ($maker = $config->runner) }
-sub store { $store || ($maker = $config->store) }
-sub webapp { $webapp || ($maker = $config->webapp) }
+sub runner { $runner || ($runner = $config->runner) }
+sub store { $store || ($store = $config->store) }
+sub webapp { $webapp || ($webapp = $config->webapp) }
 
 1;
