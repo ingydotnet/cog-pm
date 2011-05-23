c8cf2cc10ea20969ea41da536eac8f4baff18269
Ingy dot Net <ingy@ingy.net>
2011-05-23 15:00:58 +1000

    Switch to Module::Package.

---
+++ pkg/makefile.pl
@@ -0,0 +1,10 @@
+all_from $PM;
+readme_from;
+manifest_skip;
+version_check;
+ack_xxx;
+
+# Add to MIPackage. Get from .git/config
+repository 'git://github.com/ingydotnet/cog-pm.git';
+
+install_share; # Add to MIPackage
